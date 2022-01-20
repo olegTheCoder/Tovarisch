361cda026d25b0af2bb64974e202a3f5cbdf0d57
Oleg Grachev <sk8cool@mail.ru>
2022-01-20 14:17:55 +0300

    comments witn nickname

--- server/src/routes/commentRouter.js
+++ server/src/routes/commentRouter.js
@@ -4,26 +4,61 @@ const authenticateJWT = require("../middleware/authenticateJWT");
 
 router
   .route("/:id")
-  .get(authenticateJWT, async (req, res) => {
-    try {
-      let comments = [];
-      const allCommentsForIncident = await Comment.findAll({
+  .get( async (req, res) => {
+    // try {
+    //   let comments = [];
+    //   const allCommentsForIncident = await Comment.findAll({
+    //     raw: true,
+    //     where: { incidentId: req.params.id },
+    //   });
+    //   console.log("allCommentsForIncident", allCommentsForIncident);
+
+    //   for (let i = 0; i < allCommentsForIncident.length; i++) {
+    //     let personName = await User.findByPk(allCommentsForIncident[i].userId);
+    //     personName = personName.nickname;
+    //     comments.push({...allCommentsForIncident[i], nickname:personName})
+    //   }
+
+    //   console.log('comments', comments);
+    //   return res.status(200).json(comments);
+    // } catch (err) {
+    //   res.sendStatus(500);
+    // }
+
+
+try{
+  const allCommentsForIncident = await Comment.findAll({
         raw: true,
-        where: { incidentId: req.params.id },
+        where: { incidentId: req.params.id }, include: {model: User, attributes:['nickname']}
       });
-      console.log("allCommentsForIncident", allCommentsForIncident);
 
-      for (let i = 0; i < allCommentsForIncident.length; i++) {
-        let personName = await User.findByPk(allCommentsForIncident[i].userId);
-        personName = personName.nickname;
-        comments.push({...allCommentsForIncident[i], personName})
-      }
 
-      console.log('comments', comments);
-      return res.status(200).json(comments);
-    } catch (err) {
-      res.sendStatus(500);
-    }
+      // const newArr = allCommentsForIncident.map((el) => el.nickname = el['User.nickname'])
+
+      // const arrOfNicknames= allComments.map((el)=> el.nickname = el['User.nickname'])
+// const {'User.nickname': nickname } = allCommentsForIncident[0]
+
+const newArr = allCommentsForIncident.map(({'User.nickname': nickname,id, text, userId,incidentId}) => ({nickname, id, text, userId,incidentId}) )
+
+console.log(newArr);
+
+
+}catch (err) {
+    res.sendStatus(500);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
   })
   .post(authenticateJWT, async (req, res) => {
     console.log("jwt user", res.locals.user);
