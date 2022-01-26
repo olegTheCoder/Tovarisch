e84b691735c8147e8dea7a610138773d8de1458f
Oleg Grachev <91218806+olegTheCoder@users.noreply.github.com>
2022-01-26 17:44:59 +0200

    Delete client directory

--- client/src/pages/Home/Home.js
+++
@@ -1,8 +0,0 @@
-import style from './style.module.css'
-
-function Home() {
-  return <div><img src="http://localhost:3001/3333.png" className={style.bear}/></div>;
-}
-
-export default Home;
-
--- client/src/redux/sagas/rootSaga.js
+++
@@ -1,6 +0,0 @@
-import { all } from "redux-saga/effects";
-import incSagaWatcher from "./incidentSaga";
-
-export function* rootSaga() {
-  yield all([incSagaWatcher()]);
-}
